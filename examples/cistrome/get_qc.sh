4426047364226aaac3b90700121a133bf0ea5019
Ryan Layer <ryan.layer@gmail.com>
2018-04-09 14:41:14 -0600

    add qc script

--- examples/cistrome/get_qc.sh
+++ examples/cistrome/get_qc.sh
@@ -2,7 +2,7 @@
 
 echo -e "id\tmap\tpeaks\tfastqc\tfrip\tpbc\tmotif_judge\tdhs"
 
-for i in `cat data/TF_human_data_information.txt | cut -f 1`; do
+for i in `cat TF_human_data_information.txt | cut -f 1`; do
     echo -en "$i\t"
     curl -s http://dc2.cistrome.org/api/inspector?id=$i \
     | jq -cr '.qc.judge | [.map, .peaks, .fastqc, .frip, .pbc, .motif_judge, .dhs ]' \
