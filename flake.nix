a7d32db7c2840fc694bcaadf8a8d33807ca174c7
Simon Walker <simon@simonwalker.tech>
2026-03-12 22:12:57 -0600

    nix support

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            # Build tools
+            gcc
+            gnumake
+            autoconf
+            automake
+            libtool
+
+            # Libraries
+            zlib
+            bzip2
+            xz
+            curl
+            openssl
+
+            # Testing/utilities
+            bedtools
+          ];
+
+          shellHook = ''
+            export GIGGLE_ROOT=$PWD
+          '';
+        };
+      }
+    );
+}
